156875d106b5b086b429d88b3c956a8d1d9a7ade
Максим Бачинский <mbachinskiyol@gmail.com>
2021-03-17 22:27:00 +0300

    change condition url for new alpha tariff

--- src/domain/Tariffs.ts
+++ src/domain/Tariffs.ts
@@ -19,7 +19,7 @@ export let tariffs: TariffDto[] = [
     name: "Альфа-Трейдер",
     url: "",
     imageUrl: require("../assets/alfa.svg"),
-    conditionsUrl: "https://www.alfadirect.ru/uploads/%D0%A2%D0%B0%D1%80%D0%B8%D1%84%D0%BD%D1%8B%D0%B9%20%D0%BF%D0%BB%D0%B0%D0%BD%20S.pdf",
+    conditionsUrl: "https://www.alfadirect.ru/uploads/%D0%A2%D0%9F%20%D0%90%D0%BB%D1%8C%D1%84%D0%B0-%D0%A2%D1%80%D0%B5%D0%B9%D0%B4%D0%B5%D1%80.pdf",
     commissionProcent: 0.00049,
     depositoryCommissionProcent: 0.06,
     minSumToOpen: null,
