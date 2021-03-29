1ae091395db5fbe916dde5afc3217ca69df6e297
Kostya Morev <kostya@Kostyas-MacBook-Air.local>
2021-03-29 16:16:24 +0300

    add ref link to UT

--- src/domain/SpecialTariffs.ts
+++ src/domain/SpecialTariffs.ts
@@ -3,7 +3,7 @@ import { SpecialTariffDto } from "./SpecialTariffDto";
 export let specialTariffs: SpecialTariffDto[] = [
   {
     name: "Начинающий",
-    url: "",
+    url: "https://unitedtraders.com/?campaignId=420317",
     imageUrl: require("../assets/UT.svg"),
     conditionsUrl: "https://unitedtraders.com/trading",
     brokerName: "United Traders",
